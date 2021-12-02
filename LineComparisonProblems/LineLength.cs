7aa10cc9e2569c700609f484e779178dee8981ad
aamir-0143 <92806528+aamir-0143@users.noreply.github.com>
2021-12-02 18:02:28 +0530

    [Aamir8] Add ; Added Lines Are Equal Or Not

--- LineComparisonProblems/LineLength.cs
+++ LineComparisonProblems/LineLength.cs
@@ -10,6 +10,7 @@ namespace LineComparisonProblems
    {
         int x1, x2, y1, y2;
         double lengthc = 0;
+        double l1 = 0, l2 = 0;
 
         public void length()
         {
@@ -23,6 +24,34 @@ namespace LineComparisonProblems
             lengthc = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
             Console.WriteLine(lengthc);
 
+        }
+       
+        public double line1()
+        {
+            Console.WriteLine("enter the co-ordinates x1,y1,x2,y2");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("x1,y1" + " = " + x1 + "," + y1);
+            Console.WriteLine("x2,y2" + " = " + x2 + "," + y2);
+            l1 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine(l1);
+            return l1;
+
+        }
+        public double line2()
+        {
+            Console.WriteLine("enter the co-ordinates x1,y1,x2,y2");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("x1,y1" + " = " + x1 + "," + y1);
+            Console.WriteLine("x2,y2" + " = " + x2 + "," + y2);
+            l2 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+
+            return l2;
         }
 
     }
--- LineComparisonProblems/Program.cs
+++ LineComparisonProblems/Program.cs
@@ -4,11 +4,30 @@ namespace LineComparisonProblems
 {
    public class LineLengthPrograms  
    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("line comparison computation program");
+        //    LineLength c = new LineLength();
+        //    c.length();
+        //}
         static void Main(string[] args)
         {
+            Console.WriteLine("welcome to line comparison computation program");
+            double p1 = 0, p2 = 0;
             Console.WriteLine("line comparison computation program");
             LineLength c = new LineLength();
-            c.length();
+            p1 = c.line1();
+            p2 = c.line2();
+            if (p1 == p2)
+            {
+                Console.WriteLine("line are equal");
+            }
+            else
+            {
+                Console.WriteLine("line are not equal");
+            }
+
+
         }
    }
 }
