f6f1d0433b71972478766a40b74c07945c75aec6
aamir-0143 <92806528+aamir-0143@users.noreply.github.com>
2021-12-02 17:27:43 +0530

    [Aamir8] Add ; Added Length Programs

---
+++ LineComparisonProblems/LineLength.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblems
+{
+   public class LineLength 
+   {
+        int x1, x2, y1, y2;
+        double lengthc = 0;
+
+        public void length()
+        {
+            Console.WriteLine("enter the co-ordinates x1,y1,x2,y2");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("x1,y1" + " = " + x1 + "," + y1);
+            Console.WriteLine("x2,y2" + " = " + x2 + "," + y2);
+            lengthc = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine(lengthc);
+
+        }
+
+    }
+}
---
+++ LineComparisonProblems/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LineComparisonProblems 
+{
+   public class LineLengthPrograms  
+   {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("line comparison computation program");
+            LineLength c = new LineLength();
+            c.length();
+        }
+   }
+}
